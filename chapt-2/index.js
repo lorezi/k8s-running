1f337ee619282e28f5c0398166e8374b113df051
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-01-29 17:00:39 +0100

    first commit

---
+++ chapt-2/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000!");
+  console.log("http://localhost:30000");
+});
